f5a1a984ed1ffa3b08ba3faff9f0b3d16c637cd8
Rachman <rmrachmanfauzan@gmail.com>
2025-04-11 14:05:33 +0700

    update docker service

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -3,21 +3,27 @@ package db
 import (
 	"fmt"
 	"log"
-
+	"time"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+
 func InitDB() {
-	dsn := "root:localhost@tcp(127.0.0.1:3306)/elpempek?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:superadmin@tcp(db:3306)/elpempek?charset=utf8mb4&parseTime=True&loc=Local"
 
 	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("failed to connect database: %v", err)
+	for i := 0; i < 10; i++ {
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			fmt.Println("✅ Database connection established")
+			return
+		}
+		log.Printf("Retrying database connection (%d/10): %v", i+1, err)
+		time.Sleep(3 * time.Second)
 	}
 
-	fmt.Println("Database connection established")
+	log.Fatalf("❌ Failed to connect to database after retries: %v", err)
 }
